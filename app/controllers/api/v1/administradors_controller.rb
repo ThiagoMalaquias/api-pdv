5587120abbb3313fb3934eaee4a939925e60be1f
Thiago Malaquias <tammalaquias@gmail.com>
2020-09-11 00:56:08 -0300

    validade token

--- app/controllers/api/v1/administradors_controller.rb
+++ app/controllers/api/v1/administradors_controller.rb
@@ -1,48 +1,51 @@
-module Api
-  module V1
-    class AdministradorsController < ApplicationController
-      def index
-        #Todos os dados do banco da migration
-        render json: Administrador.all
-      end
-
-      def create 
-        administrador = Administrador.new(administrador_params)
-      
-        if administrador.save() #caso de tudo certo
-          render json: administrador, status: 200
-        else #caso aconteça algum erro
-          render json: { errors: administrador.errors.full_messages }, status: 400
-        end
-      end
-
-      def update 
-        administrador = Administrador.find(params[:id])
-
-        if administrador.update(administrador_params) #caso de tudo certo
-          render json: administrador, status: 202
-        else #caso aconteça algum erro
-          render json: { errors: administrador.errors.full_messages }, status: 400
-        end
-      end
-
-      def destroy 
-        administrador = Administrador.find(params[:id])
-
-        if administrador.destroy #caso de tudo certo
-          render json: administrador, status: 202
-        else #caso aconteça algum erro
-          render json: { errors: administrador.errors.full_messages }, status: 400
-        end
-      end
-
-      private 
-      
-      def administrador_params 
-        #Essa função limita os paramentros que serão aceitos pela api
-        params.require(:administrador).permit(:nome, :email, :telefone, :senha)
-      end
-      
+module Api::V1
+  class AdministradorsController < ApplicationController
+    def index
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, HEAD, DELETE'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+        
+      #Todos os dados do banco da migration
+      render json: Administrador.all
     end
+
+    def create 
+      administrador = Administrador.new(administrador_params)
+    
+      if administrador.save() #caso de tudo certo
+        render json: administrador, status: 200
+      else #caso aconteça algum erro
+        render json: { errors: administrador.errors.full_messages }, status: 400
+      end
+    end
+
+    def update 
+      administrador = Administrador.find(params[:id])
+
+      if administrador.update(administrador_params) #caso de tudo certo
+        render json: administrador, status: 202
+      else #caso aconteça algum erro
+        render json: { errors: administrador.errors.full_messages }, status: 400
+      end
+    end
+
+    def destroy 
+      administrador = Administrador.find(params[:id])
+
+      if administrador.destroy #caso de tudo certo
+        render json: administrador, status: 202
+      else #caso aconteça algum erro
+        render json: { errors: administrador.errors.full_messages }, status: 400
+      end
+    end
+
+    private 
+    
+    def administrador_params 
+      #Essa função limita os paramentros que serão aceitos pela api
+      params.require(:administrador).permit(:nome, :email, :telefone, :senha)
+    end
+    
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :valida_token_api
+
+  def valida_token_api
+    tokenValido = "7c76a78f-7123-40b1-84fd-2be5cea4131d"
+    tokenBase64 = "N2M3NmE3OGYtNzEyMy00MGIxLTg0ZmQtMmJlNWNlYTQxMzFk"
+
+    token = request.headers["Authentic-Token"]
+
+    if token.present?
+      token = Base64.decode64(token)    
+    end
+
+    if token != tokenValido
+      render json: {message: 'Token invalido ou vazio'}, status: 401
+    end
+  end
+
 end
