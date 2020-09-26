19132eb5b5072e5dc58ce21745313560fc6dc196
Rosangela Silva <roliveirasv@gmail.com>
2020-09-26 17:03:54 -0300

    Api controller Categoria

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Token.create(nome: "9d973ec6-4831-4893-bce8-bb1478dd6cfc")
-# Administrador.create(nome: "adminthiago", email: "adminthiago@pdvonline.com.br", senha: "123456")
+ Token.create(nome: "9d973ec6-4831-4893-bce8-bb1478dd6cfc")
+ Administrador.create(nome: "adminthiago", email: "adminthiago@pdvonline.com.br", senha: "123456")
 
 Empresa.create(nome: "Compre Bem", cnpj_cpf: "29.334.654/0001-33", endereco: "Rua Quatro", bairro: " Jardim Itaguaçu I", cep: "54515-54", estado: "SP", telefone: "(11)95865-5862")
 
-#Funcionario.all
+Funcionario.create(nome:"Rosangela", email:"ro@teste.com.br", cpf:"999.999.999.99",admin:"S",token:"fa1aab6a-0a8f-4cd6-9a17-d9cff8ed6e99", empresa_id:1,senha:"123456")
